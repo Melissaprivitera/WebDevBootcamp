194a0b0be70995bc8a122078642612833db5c12a
Melissa Privitera <101509873+Melissaprivitera@users.noreply.github.com>
2024-07-02 19:56:22 +0200

    exercise update

---
+++ Mod2/BBDD3/index.js
@@ -0,0 +1,9 @@
+import "./operaciones";
+
+let color1 = #aabbcc;
+let color2 = #ffaabb;
+let color3 = #dd1122;
+
+let texto = document.getElementById("contenido");
+let color = document.querySelector("color");
+
---
+++ Mod2/BBDD3/operaciones.js
@@ -0,0 +1,20 @@
+
+function sumar(a, b) {
+    return  a + b;
+}
+
+function restar(a, b) {
+   return a - b;
+}
+
+function dividir(a, b){
+    if (b != 0){
+        return a / b;
+    }else {
+        console.log("No se puede dividir por 0");
+    }
+}
+
+function multiplicar(a, b){
+    return a * b;
+}
